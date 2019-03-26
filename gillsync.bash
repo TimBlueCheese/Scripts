1869b4ef54df98dd52729dfdf5aaafad350ed77d
TimBlueCheese <48956296+TimBlueCheese@users.noreply.github.com>
2019-03-26 10:49:54 -0400

    Create gillsync.bash

---
+++ gillsync.bash
@@ -0,0 +1,4 @@
+# Adds GillCalendar.exe to startup and instructions to desktop
+
+curl https://raw.githubusercontent.com/nschnitzer/GillSync/master/GillCalendar/bin/Debug/GillCalendar.exe >~/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/GillCalendar.exe
+curl https://raw.githubusercontent.com/TimBlueCheese/Scripts/master/gillsyncnotif.txt | unix2dos >~/Desktop/How\ to\ stop\ the\ madness.txt
